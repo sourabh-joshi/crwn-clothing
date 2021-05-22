b5b6af504b084ef6bc8cadc358275f4c165afd37
Sourabh Joshi <sourabhjoshi436@gmail.com>
2021-05-22 13:21:32 +0530

    Split homepage into components, also styled menu items

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import HomePage from 'HomePage';
+import HomePage from 'pages/HomePage/HomePage';
 
-import './App.css';
+import 'App.css';
 
 const App = () => (
 	<div>
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Directory from 'components/Directory/Directory';
+
+import 'pages/HomePage/HomePage.scss';
+
+const HomePage = () => (
+	<div className="homepage">
+		<Directory />
+	</div>
+);
+
+export default HomePage;
